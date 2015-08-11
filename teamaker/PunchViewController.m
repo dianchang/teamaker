65459f16398c322ef4aba5dd639397a79a6488bb
hustlzp <hustlzp@hustlzpdeMBP.lan>
2015-08-11 16:35:22 +0800

    First commit.

---
+++ teamaker/PunchViewController.m
@@ -0,0 +1,38 @@
+//
+//  PunchViewController.m
+//  teamaker
+//
+//  Created by hustlzp on 15/8/11.
+//  Copyright (c) 2015年 hustlzp. All rights reserved.
+//
+
+#import "PunchViewController.h"
+
+@interface PunchViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation PunchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- teamaker/ViewController.m
+++ teamaker/ViewController.m
@@ -7,21 +7,76 @@
 //
 
 #import "ViewController.h"
+#import "PunchViewController.h"
+#import "CaptureViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) NSMutableArray *viewControllers;
 @end
+
+#define PAGE_COUNT 2
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self loadViewWithPage:0];
+    [self loadViewWithPage:1];
+    
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    self.scrollView.scrollsToTop = NO;
+}
+
+- (NSMutableArray *)viewControllers
+{
+    if(!_viewControllers) {
+        _viewControllers = [[NSMutableArray alloc] init];
+    }
+    return _viewControllers;
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    
+    CGRect frame = [[UIScreen mainScreen] bounds];
+    
+    self.scrollView.contentSize = CGSizeMake(frame.size.width * PAGE_COUNT, frame.size.height);
+    
+    for(int i = 0; i < self.viewControllers.count; i++) {
+        UIViewController *controller = self.viewControllers[i];
+        frame.origin.x = CGRectGetWidth(frame) * i;
+        frame.origin.y = 0;
+        controller.view.frame = frame;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)loadViewWithPage:(NSUInteger)page
+{
+    UIViewController *controller;
+    
+    switch (page) {
+        case 0:
+            controller = [[PunchViewController alloc] init];
+            break;
+        case 1:
+            controller = [[CaptureViewController alloc] init];
+            break;
+    }
+    
+    [self addChildViewController:controller];
+    [self.viewControllers insertObject:controller atIndex:page];
+    [self.scrollView addSubview:controller.view];
+    [controller didMoveToParentViewController:self];
 }
 
 @end
