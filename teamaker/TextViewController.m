13c4b0baf6c59214001a0ecb2970a466349610ea
hustlzp <hustlzp@qq.com>
2015-08-18 20:32:17 +0800

    Finish animation when keyboard show & hide in text view.

--- teamaker/TextViewController.m
+++ teamaker/TextViewController.m
@@ -10,34 +10,84 @@
 #import "Masonry.h"
 
 @interface TextViewController ()
-@property  (nonatomic, weak) UITextView *textView;
+@property (nonatomic, weak) UITextView *textView;
+@property (nonatomic, weak) UIButton *sendButton;
 @end
 
 @implementation TextViewController
 
-- (instancetype)init
+static int const sendButtonHeight = 50;
+
+- (void)loadView
 {
-    self = [super init];
+    self.view = [[UIView alloc] init];
     
-    if (self) {
-        self.view = [[UIView alloc] init];
-        
-        // 文字编辑
-        UITextView *textView = [[UITextView alloc] init];
-        [self.view addSubview:textView];
-        self.textView = textView;
-        [textView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.right.and.bottom.equalTo(self.view);
-            make.top.equalTo(self.view).with.offset(20);
-        }];
-    }
+    // 文字编辑框
+    UITextView *textView = [[UITextView alloc] init];
+    textView.font = [UIFont systemFontOfSize:20.0];
+    [self.view addSubview:textView];
+    self.textView = textView;
     
-    return self;
+    // 发送按钮
+    UIButton *sendButton = [[UIButton alloc] init];
+    self.sendButton = sendButton;
+    [sendButton setTitle:@"发送" forState:UIControlStateNormal];
+    sendButton.backgroundColor = [UIColor brownColor];
+    sendButton.titleLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:sendButton];
+    
+    [textView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.equalTo(self.view).with.offset(10);
+        make.bottom.equalTo(sendButton.mas_top);
+        make.top.equalTo(self.view).with.offset(20);
+    }];
+    
+    [sendButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.equalTo(self.view);
+        make.bottom.equalTo(self.view).offset(sendButtonHeight);
+        make.height.equalTo([NSNumber numberWithInt:sendButtonHeight]);
+    }];
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+// 键盘
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    [self.view layoutIfNeeded];
+    
+    NSDictionary *info = [notification userInfo];
+    NSValue *kbFrame = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
+    NSTimeInterval animationDuration = [[info objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    CGRect keyboardFrame = [kbFrame CGRectValue];
+    
+    CGFloat height = keyboardFrame.size.height;
+    
+    [self.sendButton mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.view).with.offset(-height);
+    }];
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification
+{
+    [self.view layoutIfNeeded];
+    
+    [self.sendButton mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.view).offset(sendButtonHeight);
+    }];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 // 重置页面
