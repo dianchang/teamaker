962eb95e230bbdac5007101708c9fb05232b1707
hustlzp <hustlzp@qq.com>
2015-08-26 15:42:35 +0800

    Add LocationDetails view.

---
+++ teamaker/Controllers/LocationDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDetailsViewController.h
+//  teamaker
+//
+//  Created by hustlzp on 15/8/26.
+//  Copyright (c) 2015年 hustlzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationDetailsViewController : UIViewController
+
+@end
